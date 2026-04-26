9052d40a67fe8fc1fa5be976c01f2b6dbde928d2
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-04-26 22:51:15 +0530

    Bug Fix v1

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -5,20 +5,15 @@ export const metadata = {
   title: "Builder | GitHub Profile Customizer",
 };
 
-import { SearchSyncBar } from "@/components/builder/SearchSyncBar";
-
 export default function BuilderPage() {
   return (
-    <div className="flex flex-col h-[calc(100vh-4rem)]">
-      <SearchSyncBar />
-      <div className="flex-1 flex flex-col lg:flex-row overflow-hidden">
-        {/* Mobile/Tablet Tab View logic can be added here if needed, but for now we'll do standard responsive stacking */}
-        <div className="w-full lg:w-96 flex-shrink-0 h-1/2 lg:h-full overflow-hidden border-b lg:border-b-0 border-slate-200 dark:border-white/10 z-10">
-          <BuilderSidebar />
-        </div>
-        <div className="flex-1 h-1/2 lg:h-full overflow-hidden relative">
-          <BuilderPreview />
-        </div>
+    <div className="flex-1 flex flex-col lg:flex-row h-[calc(100vh-4rem)] overflow-hidden">
+      {/* Mobile/Tablet Tab View logic can be added here if needed, but for now we'll do standard responsive stacking */}
+      <div className="w-full lg:w-96 flex-shrink-0 h-1/2 lg:h-full overflow-hidden border-b lg:border-b-0 border-slate-200 dark:border-white/10 z-10">
+        <BuilderSidebar />
+      </div>
+      <div className="flex-1 h-1/2 lg:h-full overflow-hidden relative">
+        <BuilderPreview />
       </div>
     </div>
   );
